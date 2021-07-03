cbd0f9d52d943ad9a172754c64c902837293b07f
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-03 19:53:33 +0600

    add heading style

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,18 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import memories from './images/memories.png';
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
+import useStyles from './styles';
 
 function App() {
+  const style = useStyles();
+
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={style.appBar} position="static" color="inherit">
+        <Typography className={style.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img className={style.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
