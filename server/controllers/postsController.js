915bedcbdc4ad9449480edc729914b674a0691f0
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-03 19:07:07 +0600

    create models for use to routes and controller

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,12 +1,40 @@
+// ? external imports
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+
 // ? internal import
+const { PostMessage } = require('../models/postMessage');
 
 // * get posts page
 
 // TODO: display posts
-const getPosts = (req, res) => {
-  res.send('This works');
+const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// TODO: create posts
+const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
 
 module.exports = {
   getPosts,
+  createPost,
 };
--- server/routes/postsRouter.js
+++ server/routes/postsRouter.js
@@ -2,11 +2,12 @@
 const express = require('express');
 
 // ? internal imports
-const { getPosts } = require('../controllers/postsController');
+const { getPosts, createPost } = require('../controllers/postsController');
 
 const router = express.Router();
 
 // TODO: post page
 router.get('/', getPosts);
+router.post('/', createPost);
 
 module.exports = router;
